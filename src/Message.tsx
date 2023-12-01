0f665dca1fcb8e208340bd826d33410f6912a955
halilolcay <holcay@marun.edu.tr>
2023-12-01 14:12:13 +0300

    initial commit

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+function Message() {
+
+    const name = 'John Doe';
+    return (
+        <div >
+            <h1>Hello {name} typescript</h1>
+        </div>
+    );
+}
+
+export default Message;
