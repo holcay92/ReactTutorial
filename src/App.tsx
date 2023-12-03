fd1a28d23322f145535fd40e0e3bbc14bd45dd7c
halilolcay <holcay@marun.edu.tr>
2023-12-03 22:07:16 +0300

    function as a paramter to a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import ListGroup from "./components/ListGroup";
+function App() {
+  let items = [
+    "Niğde",
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Portland",
+    "Las Vegas",
+    "Memphis",
+    "North Las Vegas",
+    "Scottsdale",
+    "Baton Rouge",
+    "Fremont",
+    "Richmond",
+    "Boise",
+    "San Bernardino",
+  ];
 
-import ListGroup from './components/ListGroup';
-function App (){
-    return (
-        <div>
-            <ListGroup/>
-        </div>
-    )
+  const handleClick = (item: string) => console.log(item);
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="New Cities"
+        onItemSelect={handleClick}
+      />
+    </div>
+  );
 }
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,118 +1,18 @@
 import React, { MouseEvent, useState } from "react";
-function ListGroup() {
-  let items = [
-    "New York",
-    "Los Angeles",
-    "Chicago",
-    "Houston",
-    "Phoenix",
-    "Philadelphia",
-    "San Antonio",
-    "San Diego",
-    "Dallas",
-    "San Jose",
-    "Austin",
-    "Jacksonville",
-    "Fort Worth",
-    "Columbus",
-    "San Francisco",
-    "Charlotte",
-    "Indianapolis",
-    "Seattle",
-    "Denver",
-    "Washington",
-    "Boston",
-    "El Paso",
-    "Nashville",
-    "Detroit",
-    "Oklahoma City",
-    "Portland",
-    "Las Vegas",
-    "Memphis",
-    "Louisville",
-    "Baltimore",
-    "Milwaukee",
-    "Albuquerque",
-    "Tucson",
-    "Fresno",
-    "Sacramento",
-    "Mesa",
-    "Kansas City",
-    "Atlanta",
-    "Long Beach",
-    "Colorado Springs",
-    "Raleigh",
-    "Miami",
-    "Virginia Beach",
-    "Omaha",
-    "Oakland",
-    "Minneapolis",
-    "Tulsa",
-    "Arlington",
-    "New Orleans",
-    "Wichita",
-    "Cleveland",
-    "Tampa",
-    "Bakersfield",
-    "Aurora",
-    "Honolulu",
-    "Anaheim",
-    "Santa Ana",
-    "Corpus Christi",
-    "Riverside",
-    "St. Louis",
-    "Lexington",
-    "Pittsburgh",
-    "Stockton",
-    "Anchorage",
-    "Cincinnati",
-    "Saint Paul",
-    "Greensboro",
-    "Toledo",
-    "Newark",
-    "Plano",
-    "Henderson",
-    "Lincoln",
-    "Orlando",
-    "Jersey City",
-    "Chula Vista",
-    "Buffalo",
-    "Fort Wayne",
-    "Chandler",
-    "St. Petersburg",
-    "Laredo",
-    "Durham",
-    "Irvine",
-    "Madison",
-    "Norfolk",
-    "Lubbock",
-    "Gilbert",
-    "Winston–Salem",
-    "Glendale",
-    "Reno",
-    "Hialeah",
-    "Garland",
-    "Chesapeake",
-    "Irving",
-    "North Las Vegas",
-    "Scottsdale",
-    "Baton Rouge",
-    "Fremont",
-    "Richmond",
-    "Boise",
-    "San Bernardino",
-  ];
-  //items = [];
+
+interface ListGroupProps {
+  items: string[];
+  heading: string;
+  onItemSelect: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onItemSelect }: ListGroupProps) {
   const handleClick = (event: MouseEvent) => console.log(event);
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  /*   const getEmptyMessage = () => {
-    return items.length === 0 ? <p>There are no items in the list.</p> : null;
-  }; */
-
   return (
     <ul className="list-group">
-      <h1>List</h1>
+      <h1>{heading}</h1>
       {/*getEmptyMessage()*/}
       {/* it is the same as above with short circuit operator */}
       {items.length === 0 && <p>There are no items in the list.</p>}
@@ -126,7 +26,7 @@ function ListGroup() {
           key={index}
           onClick={() => {
             setSelectedIndex(index);
-            handleClick;
+            onItemSelect(item);
           }}
         >
           {item}
